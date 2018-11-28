262940406eb606b220cae461f5d300391b9c7d9b
Lin-z Chang <lin-zchang@amrock.com>
2018-11-28 14:39:59 -0500

    Added fields to registration form, sends user object to Result page

--- CoffeeShop/Controllers/HomeController.cs
+++ CoffeeShop/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace GC_CoffeeShop.Controllers
+namespace CoffeeShop.Controllers
 {
     public class HomeController : Controller
     {
