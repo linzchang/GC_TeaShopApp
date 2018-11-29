64fbbba1de17220889596358972cec84266b2e82
Lin-z Chang <lin-zchang@amrock.com>
2018-11-29 14:45:26 -0500

    Fixed Javascript and changed some styling

--- CoffeeShop/Scripts/Register.js
+++ CoffeeShop/Scripts/Register.js
@@ -2,12 +2,9 @@
     
     $("#register_submit").click(function (event) {
         var phoneNumber = $("#phone").val();
-        alert(phoneNumber);
         var error_free = true;
         var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-        
         var isPhone = re.test(phoneNumber);
-        alert(isPhone);
         if (!isPhone)
         {
             error_free = false;
@@ -15,9 +12,6 @@
         if (!error_free) {
             event.preventDefault();
             alert('Please enter a valid phone number!');
-        }
-        else {
-            alert('No errors: Form will be submitted');
         }
     });
 
