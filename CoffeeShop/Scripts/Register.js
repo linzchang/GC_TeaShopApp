d19745161d39f60b49aa68451c0863721009bbf4
Lin-z Chang <lin-zchang@amrock.com>
2018-11-29 09:07:28 -0500

    Added Javascript to validate Phone Number field

---
+++ CoffeeShop/Scripts/Register.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    
+    $("#register_submit").click(function (event) {
+        var phoneNumber = $("#phone").val();
+        alert(phoneNumber);
+        var error_free = true;
+        var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+        
+        var isPhone = re.test(phoneNumber);
+        alert(isPhone);
+        if (!isPhone)
+        {
+            error_free = false;
+        }
+        if (!error_free) {
+            event.preventDefault();
+            alert('Please enter a valid phone number!');
+        }
+        else {
+            alert('No errors: Form will be submitted');
+        }
+    });
+
+});
+
