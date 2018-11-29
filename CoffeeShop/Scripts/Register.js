44513b4690d8a9725cc3fa032601589ec8aeea5e
Lin-z Chang <lin-zchang@amrock.com>
2018-11-29 16:59:00 -0500

    Added Javascript to validate password, also changed Home page and added images

--- CoffeeShop/Scripts/Register.js
+++ CoffeeShop/Scripts/Register.js
@@ -11,8 +11,40 @@
         }
         if (!error_free) {
             event.preventDefault();
-            alert('Please enter a valid phone number!');
+            $("#phoneAlert").text('Please enter a valid phone number')
         }
+    });
+
+    $('input[type=password]').keyup(function () {
+        var password = $(this).val();
+        //check password length
+        if (password.length < 8) {
+            $('#length').removeClass('text-success').addClass('text-danger');
+        } else {
+            $('#length').removeClass('text-danger').addClass('text-success');
+        }
+        //check if password has at least one letter
+        if (password.match(/[A-z]/)) {
+            $('#letter').removeClass('text-danger').addClass('text-success');
+        } else {
+            $('#letter').removeClass('text-success').addClass('text-danger');
+        }
+        //check if password has a capital letter
+        if (password.match(/[A-Z]/)) {
+            $('#capital').removeClass('text-danger').addClass('text-success');
+        } else {
+            $('#capital').removeClass('text-success').addClass('text-danger');
+        }
+        //check if password has at least one number
+        if (password.match(/\d/)) {
+            $('#number').removeClass('text-danger').addClass('text-success');
+        } else {
+            $('#number').removeClass('text-success').addClass('text-danger');
+        }
+    }).focus(function () {
+        $('#passwordInfo').show();
+    }).blur(function () {
+        $('#passwordInfo').hide();
     });
 
 });
